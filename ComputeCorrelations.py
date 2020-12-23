c117f080d10f1a95918f3a54f16fa82001e4ae4e
Afaf saaidi <afaf.saaidi@gmail.com>
2020-12-23 13:09:32 -0700

    restrictions for the code

--- ComputeCorrelations.py
+++ ComputeCorrelations.py
@@ -1,3 +1,7 @@
+### As the project is under development, you cannot access the code of the "ComputeCorelation" method.
+### Please contact me to get the full version.
+
+#@asaaidi 2020
 # -*- coding: utf-8 -*-
 import numpy as np
 import math
@@ -16,131 +20,6 @@ parser.add_argument("--fasta", "-R", type=str, required=True)
 parser.add_argument("--amplicon", "-a",  type=int, required=True)
 args = parser.parse_args()
 
-def Computecorrelations(MM,UU,Prior,Pairs,lenRNA,Name,MinLenHelix,setofhelices):
-	Norm= np.zeros((lenRNA,lenRNA)) 
-	for i in range( len(MM)):
-		for j in range(len(MM)):
-			if Prior[i,j]!=0:
-				Norm[i,j]=(MM[i,j]-Prior[i,j])/float(Prior[i,j])
-	#set diagonals to Zero
-        for i in range( len(MM)):
-		MM[i,i]=np.nan
-		Norm[i,i]=0
-	
-	Norm[Norm==0]=np.nan # set null normalized values to NA because Norm was initially defined as matrix of zeros
-	######### Output MM count and Rltdiff
-	with open (os.path.join(OutputFolder,(Name.split("/")[-1]).split(".")[0]+"_Rawdata.csv"), 'w') as outfile: 
-		outfile.write("%s,%s,%s,%s,%s\n"% ( "i","j","ncts","MM","Rltdiff" ))
-		for i in range( Amplicon, lenRNA-Amplicon):
-			for j in range( i+7,lenRNA-Amplicon):
-				outfile.write("%i,%i,%s,%.4f,%.4f \n"% (i+1,j+1,str(seq[i]+seq[j]),round(MM[i,j],4),round(Norm[i,j],4)))
-	outfile.close()
-	######## output data for specific helices
-	'''
-	outputcorrelation=os.path.join(OutputFolder,(Name.split("/")[-1]).split(".")[0]+"_Norm_Featured_helices.csv")
-	Lista=[]
-	for Helix in setofhelices:
-		if Helix[1][2]>MinLenHelix: 	
-			listtuple=[]	
-			i=Helix[1][0]-1
-			j=Helix[1][1]-1
-			l=Helix[1][2]
-			for k in range(l):# neigborhood [i-1,i+1]x[j-1,j+1]
-				for (n,t) in [(i+k,j-k),(i+k,j-k-1),(i+k,j-k+1),(i+k-1,j-k),(i+k-1,j-k-1),(i+k-1,j-k+1),(i+k+1,j-k),(i+k+1,j-k-1),(i+k+1,j-k+1)]:
-					if j+1<lenRNA and math.isnan(Norm[n,t])!=True and (n,t,Norm[n,t])not in listtuple:  
-						Lista.append((Helix[0],n,t,Norm[n,t],l))
-						#Listadit.append(((Helix[0],l),n,t,Norm[n,t]))
-						listtuple.append((n,t,Norm[n,t]))	
-	
-        # Get the #of common pairs between helices NA not included.
-	dicto= defaultdict()
-	dictobis= defaultdict() #oneadditional element that is the length of the helix
-	CommonPairs=defaultdict()
-	#SortedAverageHelix=defaultdict()
-	#initialization
-	for elem in Lista:
-		dicto[elem[0]]=list()
-		dictobis[elem[0]]=list()
-		CommonPairs[elem[0]]=list()	
-	#incrementation
-	for elem in Lista:
-		dicto[elem[0]].append((elem[1],elem[2],elem[3]))
-		dictobis[elem[0]].append((elem[1],elem[2],elem[3],elem[4]))
-	Averagehelix=defaultdict() 
-	for key in dictobis:
-		
-		toaverage=[]
-		## CC content and AA content 
-		listAA=0
-		listCC=0
-		for elem in dictobis[key]:
-			# verification if key=="eP19plus1BP":
-			#	print elem, seq[elem[0]],seq[elem[1]]
-			if seq[elem[0]].upper()=="C" and seq[elem[1]].upper()=="C":
-				listCC+=1
-			if seq[elem[0]].upper()=="A" and seq[elem[1]].upper()=="A":
-				listAA+=1
-			#print "loloo",elem[0],seq[elem[0]],seq[elem[1]]
-			toaverage.append(elem[2])
-		#tuple of average, nbr of observations, expected number of observations
-		Averagehelix[key]=(round(np.mean(toaverage),3),len(toaverage),dictobis[key][0][3]*5+4,listCC,listAA)
-	
-	
-	
-	#1 ----------first filtering layer not enough data, the nbr of observations is strctely less than the half of the expected number of observations 
-	for elem in Averagehelix.keys():
-		
-		if Averagehelix[elem][1]<Averagehelix[elem][2]/2:
-			del Averagehelix[elem]
-	#print "After first filtering", Averagehelix
-	#print Averagehelix
-	#2 ------------ look for common pairs and remove those that are dominated by other helices
-	for key1 in Averagehelix:
-		firstlist=[elem for elem in dicto[key1]]
-		for key2 in Averagehelix:
-			if key2!=key1:
-				secondlist=[elem for elem in dicto[key2]]
-				#get pairs in common btw helices
-				
-				if len(set(firstlist) & set(secondlist))!=0:
-					CommonPairs[key1].append((key2,len(set(firstlist) & set(secondlist))))
-	#print  set(firstlist) ,"\n",  set(secondlist),"\n", set(firstlist) & set(secondlist)
-	#print CommonPairs	
-	removed=[]
-	for Helix in Averagehelix:
-		for Helixrelated in CommonPairs[ Helix]:
-			Helice=Helixrelated[0]	
-			commonpairs=Helixrelated[1]
-			# if dominated by another helix in average and most than half of observed values are in common, remove this helix
-			#print Averagehelix[Helice][0],Averagehelix[Helix][0],commonpairs,Averagehelix[Helix][1],Averagehelix[Helice][0]<Averagehelix[Helix][0] , commonpairs>=Averagehelix[Helix][1]
-			if Averagehelix[Helix][0]<Averagehelix[Helice][0] and 2*commonpairs>=Averagehelix[Helix][1]:
-				
-				removed.append(Helix)
-	for Helix in list(set(removed)): # a trick to remove redundant helices
-		if Averagehelix[Helix]:
-			del Averagehelix[Helix]
-			
-	#sort the dictionary:
-	SortedAverageHelix = Averagehelix.items()
-	SortedAverageHelix.sort(key=lambda x:-x[1][0])
-	#print  "initila,",SortedAverageHelix
-	#print "Averagehelix \n",Averagehelix	
-		
-	with open (outputcorrelation, 'w') as outfile:
-		outfile.write("%s,%s,%s,%s,%s,%s,%s,%s\n"% ( "Dataset","Helix","i","j","seqij","Rltdiff","ijdist","commonpairs" ))
-		for elem in Lista:
-			outfile.write("%s,%s,%s,%s,%s,%.4f,%i,%s \n"% ( (Name.split("/")[-1]).split("_")[1],elem[0],str(elem[1]+1),str(elem[2]+1),str(seq[elem[1]]+seq[elem[2]]),round(elem[3],4),elem[2]-elem[1]+1,CommonPairs[elem[0]]))
-	'''
-	Order=[]
-	'''
-	with open (outputcorrelation, 'w') as outfile:
-		outfile.write("%s,%s,%s,%s,%s,%s\n"% ( "Helix","averageRLtdiff","nbrobservations","Length","nbrCCpairs","nbrAApairs" ))
-		for elem in SortedAverageHelix:
-			Order.append(elem[0])
-			outfile.write("%s,%.4f,%i,%i,%i,%i \n"% (elem[0], elem[1][0] , elem[1][1], (elem[1][2]-4)/5, elem[1][3], elem[1][4]))
-	outfile.close()
-	'''
-	return Order
 def ListBasePairsFromStruct(Struct):  # return dic={structure:[liste de pairs de base ],....}
     lista = []
     stack = []
@@ -178,21 +57,7 @@ def classification(mutationrate):
 	return R
 	
 	("P3a",(21,68,8))
-	
-def EmbeddedHelices(listofHelices): # Embedded helices are defined as follow: H2 is inside the space [H1+L1,H1-L1] the maximal distance is two nucleotides from each side.
-	List=[]
-	#listofHelices=[("Cx",(79,97,8)),("Cx2",(83,92,4)),("Cx3",(80,98,4))]
-	for H1 in listofHelices:
-		for H2 in listofHelices:
-			#print  H1,H2,H2[1][0]>H1[1][0],H2[1][0]-(H1[1][0]+H1[1][2])<3,H1[1][1]-H1[1][2]-H2[1][1]<3
-			if H2[1][0]>=H1[1][0]+ H1[1][2] and H2[1][1]<=H1[1][1]- H1[1][2]and  H2[1][0]-H1[1][0]-H1[1][2]<3 and H1[1][1]-H1[1][2]-H2[1][1]<3:
-				# length of the new embeededhelix is the sum of the two helices lengths and the maximal gap value that is less than 3
-				LengthEmbeddedhelix=H1[1][2]+H2[1][2]+np.amax([H2[1][0]-H1[1][0]-H1[1][2],H1[1][1]-H1[1][2]-H2[1][1]])
-				List.append((str(H1[0]+H2[0]),(H1[1][0],H1[1][1],LengthEmbeddedhelix)))
-	print "List embeeded", List
-	return List
 
-    
 if __name__=="__main__":
 	#python2.7 ComputeCorrelationsMaximalhelicesElimination.py --file Incell/Modified/5S_INCELL1_5SFREQUENCY_COUNTex.txt  --output 2020-10-21 --fasta alignment_sequences/5S.fa --amplicon 20 --p /home/user/Documents/GeorgiaTech2020/Generate_normalized_prior_Input/2020-10-20/Profiling/5SProfiling.csv
 	setofhelices=[]
